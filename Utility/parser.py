c3c04d8af94d3b0242bfec072c0e3187a4462c5f
jskacines <jskacines@gmail.com>
2024-10-05 10:24:22 -0500

    updated vehicle yaml to include most parameters

--- Utility/parser.py
+++ Utility/parser.py
@@ -1,5 +1,6 @@
 import yaml
 import pint
+import string
 
 def read_yaml(file:str):
     with open(file,'r') as input:
@@ -12,15 +13,22 @@ def convert(params:dict):
         ureg.meter,
         ureg.kilogram,
         ureg.second,
-        ureg.newton
+        ureg.newton,
+        ureg.radian, #this does nothing
     ]
 
     kgms_params_list = []
     print(params)
     for key, value in params.items():
-        value_adjusted = ureg.Quantity(value)
-        value_adjusted = value_adjusted.to_preferred()
-        print(f'{key}: {ureg.Quantity(value)} -> {value_adjusted}')
-        kgms_params_list.append((key,value_adjusted.magnitude))
-        kgms_params = dict(kgms_params_list)
-    return kgms_params
+        try:
+            value_raw = ureg.Quantity(value)
+            value_adjusted = value_raw.to_preferred()
+            if value_raw.units==('degree'): #TODO make more robust
+                value_adjusted=value_raw.to('radian')
+            print(f'{key}: {ureg.Quantity(value_raw)} -> {value_adjusted}')
+            kgms_params_list.append((key,value_adjusted.magnitude))
+        except (ValueError, TypeError):
+            kgms_params_list.append((key,value))
+    kgms_params = dict(kgms_params_list)
+    print(kgms_params)
+    return kgms_params # FYI | deg -> rad | rpm -> rad/s 
