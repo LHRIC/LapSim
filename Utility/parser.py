7ee03c81fd23ef0811a66e442fb1a2b9f7a41ad4
Nathan Nguyen <145217084+nathannguyen4@users.noreply.github.com>
2025-01-18 12:34:53 -0600

    Update parser.py

--- Utility/parser.py
+++ Utility/parser.py
@@ -2,37 +2,6 @@ import yaml
 import pint
 import string
 
-def read_yaml(file:str):
-    with open(file,'r') as input:
-        data = yaml.safe_load(input)
-    return data
-
-def convert(params:dict):
-    ureg = pint.UnitRegistry(autoconvert_to_preferred=False)
-    ureg.default_preferred_units = [
-        ureg.meter,
-        ureg.kilogram,
-        ureg.second,
-        ureg.newton,
-        ureg.radian, #this does nothing
-    ]
-
-    kgms_params_list = []
-    print(params)
-    for key, value in params.items():
-        try:
-            value_raw = ureg.Quantity(value)
-            value_adjusted = value_raw.to_preferred()
-            if value_raw.units==('degree'): #TODO make more robust
-                value_adjusted=value_raw.to('radian')
-            print(f'{key}: {ureg.Quantity(value_raw)} -> {value_adjusted}')
-            kgms_params_list.append((key,value_adjusted.magnitude))
-        except:
-            kgms_params_list.append((key,value))
-    kgms_params = dict(kgms_params_list)
-    print(kgms_params)
-    return kgms_params # FYI | deg -> rad | rpm -> rad/s 
-
 def parse_tir(filepath):
     parsed_data = {}
 
